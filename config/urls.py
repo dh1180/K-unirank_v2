84bf403530cd3ebaf23d4adc5db6f58c14fc6933
김동현 <53690235+dh1180@users.noreply.github.com>
2026-08-23 23:46:45 +0900

    feat: sitemap.xml과 robots.txt 제공

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.contrib.sitemaps.views import sitemap
 from django.http import HttpResponse
 from django.urls import include, path
 
 from admissions import filter_views as admission_filter_views
 from rankings.views import health
 from users.forms import LoginForm
+
+from .sitemaps import sitemaps
 
 
 def ads_txt(request):
@@ -15,10 +18,32 @@ def ads_txt(request):
     )
 
 
+def robots_txt(request):
+    lines = [
+        "User-agent: *",
+        "Allow: /",
+        "Disallow: /admin/",
+        "Disallow: /accounts/",
+        "Disallow: /api/",
+        "Disallow: /health/",
+        "Disallow: /result/",
+        "",
+        "Sitemap: https://www.k-unirank.com/sitemap.xml",
+    ]
+    return HttpResponse("\n".join(lines) + "\n", content_type="text/plain; charset=utf-8")
+
+
 urlpatterns = [
     # v84: 입결 찾기를 사이트의 canonical root/home으로 사용
     path("", admission_filter_views.overview, name="home"),
     path("ads.txt", ads_txt, name="ads_txt"),
+    path("robots.txt", robots_txt, name="robots_txt"),
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
     path("admin/", admin.site.urls),
     path(
         "accounts/login/",
