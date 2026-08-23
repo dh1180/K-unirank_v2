88fb767608646bf024d53edff1b2286c731ad604
김동현 <53690235+dh1180@users.noreply.github.com>
2026-08-23 23:53:31 +0900

    feat: 학과·모집단위별 입결 고유 URL 추가

--- admissions/urls.py
+++ admissions/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.views.generic import RedirectView
 
-from . import filter_views, track_ui_views, views
+from . import filter_views, seo_views, track_ui_views, views
 
 
 app_name = "admissions"
@@ -15,5 +15,6 @@ urlpatterns = [
     ),
     path("results/", filter_views.overview_results, name="overview_results"),
     path("ranking/", views.admission_ranking, name="ranking"),
+    path("unit/<int:recruitment_unit_id>/", seo_views.recruitment_unit_detail, name="unit"),
     path("university/<int:university_id>/", track_ui_views.university_admissions, name="university"),
 ]
