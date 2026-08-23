9a51fa0fb66027a0940ebf524036676bb23842e4
김동현 <53690235+dh1180@users.noreply.github.com>
2026-08-23 16:50:56 +0900

    feat: 마이페이지와 관심 토글 URL 추가

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,17 @@ from . import views
 
 
 app_name = "users"
-urlpatterns = [path("signup/", views.signup, name="signup")]
+urlpatterns = [
+    path("signup/", views.signup, name="signup"),
+    path("me/", views.mypage, name="mypage"),
+    path(
+        "favorites/university/<int:university_id>/toggle/",
+        views.toggle_favorite_university,
+        name="toggle_favorite_university",
+    ),
+    path(
+        "favorites/recruitment-unit/<int:recruitment_unit_id>/toggle/",
+        views.toggle_favorite_recruitment_unit,
+        name="toggle_favorite_recruitment_unit",
+    ),
+]
