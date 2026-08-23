1f25d0943c0bd29a5ff25f6b931646fa4c0b02c9
김동현 <53690235+dh1180@users.noreply.github.com>
2026-08-23 17:54:11 +0900

    feat: VS 전체 내역 페이지 라우트 추가

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,7 @@ app_name = "users"
 urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("me/", views.mypage, name="mypage"),
+    path("me/vs-history/", views.vs_history, name="vs_history"),
     path(
         "favorites/university/<int:university_id>/toggle/",
         views.toggle_favorite_university,
