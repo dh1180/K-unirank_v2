b30e1891cf675a9df2ce1f082d68b0a71d55e4d3
김동현 <53690235+dh1180@users.noreply.github.com>
2026-09-09 14:10:17 +0900

    feat: 관리자에서 수능최저 요구사항 확인 지원

--- admissions/admin.py
+++ admissions/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
-from .models import AdmissionAggregate, AdmissionMetric, AdmissionResult, AdmissionSource, RecruitmentUnit
+from .models import (
+    AdmissionAggregate,
+    AdmissionMetric,
+    AdmissionRequirement,
+    AdmissionResult,
+    AdmissionSource,
+    RecruitmentUnit,
+)
 
 
 class AdmissionMetricInline(admin.TabularInline):
@@ -10,10 +17,42 @@ class AdmissionMetricInline(admin.TabularInline):
 
 @admin.register(AdmissionResult)
 class AdmissionResultAdmin(admin.ModelAdmin):
-    list_display = ("university", "admission_year", "admission_phase", "selection_category", "recruitment_unit", "competition_rate")
+    list_display = (
+        "university",
+        "admission_year",
+        "admission_phase",
+        "selection_category",
+        "recruitment_unit",
+        "competition_rate",
+    )
     list_filter = ("admission_year", "admission_phase", "selection_category")
     search_fields = ("university__name", "recruitment_unit__name", "selection_name")
     inlines = [AdmissionMetricInline]
+
+
+@admin.register(AdmissionRequirement)
+class AdmissionRequirementAdmin(admin.ModelAdmin):
+    list_display = (
+        "university",
+        "admission_year",
+        "requirement_type",
+        "selection_name",
+        "recruitment_unit_name",
+        "applied",
+        "source_type",
+    )
+    list_filter = (
+        "admission_year",
+        "requirement_type",
+        "applied",
+        "source_type",
+    )
+    search_fields = (
+        "university__name",
+        "selection_name",
+        "recruitment_unit_name",
+        "requirement_text",
+    )
 
 
 admin.site.register(RecruitmentUnit)
